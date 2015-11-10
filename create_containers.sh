b1447a87943956c25e8824bdcb9cfb3202f34f17
Hiroaki Nakamura <hnakamur@gmail.com>
2015-11-11 06:32:22 +0900

    Create CentOS7 container for LXC 1.1.x only

--- create_containers.sh
+++ create_containers.sh
@@ -2,9 +2,14 @@
 set -e
 set -x
 
+use_lxc_1_1=${USE_LXC_1_1:-0}
+
 lxc-create -n trusty ct -t download -- -d ubuntu -r trusty -a amd64
 lxc-start -dn trusty
-lxc-create -n cent7 ct -t download -- -d centos -r 7 -a amd64
-lxc-start -dn cent7
 lxc-create -n cent6 ct -t download -- -d centos -r 6 -a amd64
 lxc-start -dn cent6
+
+if [ $use_lxc_1_1 -ne 0 ]; then
+  lxc-create -n cent7 ct -t download -- -d centos -r 7 -a amd64
+  lxc-start -dn cent7
+fi
